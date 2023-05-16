7b0406b6c4abd22f3a872caccaa8e4c3683caefa
developerjini <i5veagl@gmail.com>
2023-05-16 12:20:33 +0900

    Create Todo 구현 퍼시스턴스 구현, 서비스 구현 코드 리팩토링

--- hello/src/main/java/com/developerjini/hello/service/TodoService.java
+++ hello/src/main/java/com/developerjini/hello/service/TodoService.java
@@ -1,11 +1,16 @@
 package com.developerjini.hello.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.developerjini.hello.model.TodoEntity;
 import com.developerjini.hello.persistence.TodoRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class TodoService {
   // public String testService() {
@@ -25,4 +30,27 @@ public class TodoService {
     return savedEntity.getTitle();
   }
 
+  public List<TodoEntity> create(final TodoEntity entity) {
+    // Validations
+    validate(entity);
+
+    repository.save(entity);
+
+    log.warn("Entity Id : {} is saved", entity.getId());
+
+    return repository.findByUserId(entity.getUserId());
+  }
+
+  private void validate(final TodoEntity entity) {
+
+    if (entity == null) {
+      log.warn("Entity cannot be null.");
+      throw new RuntimeException("Entity cannot be null");
+    }
+
+    if (entity.getUserId() == null) {
+      log.warn("Unknown User");
+      throw new RuntimeException("Unknown user");
+    }
+  }
 }
