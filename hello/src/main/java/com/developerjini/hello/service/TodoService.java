26d8820d6e3013e0a5ee9beb6fa12c1a234935f6
developerjini <i5veagl@gmail.com>
2023-05-16 11:59:11 +0900

    TodoService에서 TodoRepository 사용하기

--- hello/src/main/java/com/developerjini/hello/service/TodoService.java
+++ hello/src/main/java/com/developerjini/hello/service/TodoService.java
@@ -1,10 +1,28 @@
 package com.developerjini.hello.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.developerjini.hello.model.TodoEntity;
+import com.developerjini.hello.persistence.TodoRepository;
 
 @Service
 public class TodoService {
+  // public String testService() {
+  // return "test Service";
+  // }
+
+  @Autowired
+  private TodoRepository repository;
+
   public String testService() {
-    return "test Service";
+    // TodoEntity 생성
+    TodoEntity entity = TodoEntity.builder().title("My First todo item").build();
+    // TodoEntity 저장
+    repository.save(entity);
+    // TodoEntity 검색
+    TodoEntity savedEntity = repository.findById(entity.getId()).get();
+    return savedEntity.getTitle();
   }
+
 }
