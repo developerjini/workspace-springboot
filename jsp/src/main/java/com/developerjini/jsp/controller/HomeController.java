c0e52d7c9aedb5fde8ba795c5cb35013a2a6388d
developerjini <i5veagl@gmail.com>
2023-05-17 11:08:34 +0900

    index.jsp 연결 성공

--- jsp/src/main/java/com/developerjini/jsp/controller/HomeController.java
+++ jsp/src/main/java/com/developerjini/jsp/controller/HomeController.java
@@ -2,9 +2,11 @@ package com.developerjini.jsp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+  @GetMapping("/")
   public String home(Model model) {
     model.addAttribute("name", "myName");
     return "index";
--- jsp/src/main/webapp/WEB-INF/view/index.jsp
+++ jsp/src/main/webapp/WEB-INF/view/index.jsp
@@ -1,4 +1,4 @@
-<%@ contentType="text/html; charset=utf-8" pageEncoding="utf-8" %>
+<%@ page contentType="text/html; charset=utf-8" pageEncoding="utf-8" %>
 <!DOCTYPE html>
 <html>
 <head>
