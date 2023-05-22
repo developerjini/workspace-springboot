894a6eb7b79063c6237f184f81ba65725fe76491
developerjini <i5veagl@gmail.com>
2023-05-22 12:02:51 +0900

    gitignore 내용 추가

---
+++ hello/src/main/java/com/developerjini/hello/persistence/UserRepository.java
@@ -0,0 +1,15 @@
+package com.developerjini.hello.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.developerjini.hello.model.UserEntity;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+  UserEntity findByUsername(String username);
+
+  Boolean existsByUsername(String username);
+
+  UserEntity findByUsernameAndPassword(String username, String password);
+}
